26d1effe13ba4248b1d3de80b046cb861f285923
Sandro Magi <none@none>
2018-03-08 19:51:57 -0500

    rename overload to override

--- Google.Cloud.Datastore.V1.Mapper/Value.cs
+++ Google.Cloud.Datastore.V1.Mapper/Value.cs
@@ -38,7 +38,7 @@ namespace Google.Cloud.Datastore.V1.Mapper
                     ?? typeof(Value).GetMethods(BindingFlags.Static | BindingFlags.Public)
                                     .SingleOrDefault(x => x.ReturnType == type && "op_Explicit".Equals(x.Name, StringComparison.Ordinal));
             if (to != null && from != null)
-                Overload((Func<Value, T>)from.CreateDelegate(typeof(Func<Value, T>)),
+                Override((Func<Value, T>)from.CreateDelegate(typeof(Func<Value, T>)),
                          (Func<T, Value>)to.CreateDelegate(typeof(Func<T, Value>)));
             else if (to != null || from != null)
                 throw new Exception("Type " + type.Name + " has only one conversion but needs both.");
@@ -49,7 +49,7 @@ namespace Google.Cloud.Datastore.V1.Mapper
         /// </summary>
         /// <param name="from"></param>
         /// <param name="to"></param>
-        public static void Overload(Func<Value, T> from, Func<T, Value> to)
+        public static void Override(Func<Value, T> from, Func<T, Value> to)
         {
             From = from ?? throw new ArgumentNullException(nameof(from));
             To = to ?? throw new ArgumentNullException(nameof(to));
