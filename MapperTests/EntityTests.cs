c60062fec7485c8a09e0cd5f08b688d8a0fd00c3
Sandro Magi <none@none>
2018-04-14 17:29:40 -0400

    fixed tests

--- MapperTests/EntityTests.cs
+++ MapperTests/EntityTests.cs
@@ -29,8 +29,8 @@ namespace MapperTests
         public static void SimpleTests()
         {
             var x = new Simple { Bar = 99, Baz = "hello world!" };
-            var e = Entity<Simple>.To(x, new Entity());
-            var y = Entity<Simple>.From(e, new Simple());
+            var e = Entity<Simple>.To(new Entity(), x);
+            var y = Entity<Simple>.From(new Simple(), e);
             Assert.Equal(x.Bar, y.Bar);
             Assert.Equal(x.Baz, y.Baz);
             Assert.Equal(x.Bar, e["Bar"]);
@@ -49,8 +49,8 @@ namespace MapperTests
                 Amount = 987654321M,
                 IO = new MemoryStream(Encoding.ASCII.GetBytes("hello world!")),
             };
-            var e = Entity<Complex>.To(x, new Entity());
-            var y = Entity<Complex>.From(e, new Complex());
+            var e = Entity<Complex>.To(new Entity(), x);
+            var y = Entity<Complex>.From(new Complex(), e);
             Assert.Equal(x.Id, y.Id);
             Assert.Equal(x.Uri, y.Uri);
             Assert.Equal(x.Amount, y.Amount);
